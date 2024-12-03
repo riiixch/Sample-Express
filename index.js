a64bf0132cbe320e88909e33495670c73a3868c7
RIIIXCH <bigowne.server.netmail@gmail.com>
2024-12-04 01:23:44 +0700

    Update

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ app.use('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/api/send-data', (req, res) => {
-    const { message } = req.body;
-
-    log(`Received message: ${message}`);
-});
-
 app.listen(8080, () => {
     log('[ExpressJS] Website listen port http://localhost:8080/');
 });
